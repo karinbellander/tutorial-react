7e6568e12e828cc4e57e474cae41c2d9ec8a2ef1
karinbellander <karinbellander@gmail.com>
2018-07-19 17:57:09 +0200

    Lesson 1,2

---
+++ learn-react-by-building-a-web-app-master/react-coin/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Komponent, javascript funktion
+const App = () => {
+    return <h1>React Coin</h1>
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
